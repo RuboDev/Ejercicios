2cbc8a1a4fa1e53138e7bb95cb0ddc7e193ff137
Rubén <rubololer@gmail.com>
2021-04-27 18:25:31 +0200

    Agrego paquete Ejercicio6

---
+++ Ud08-POO2/Ejercicio6/DemasiadoRapidoException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio6;
+
+/**
+ *
+ * @author Ruben
+ */
+public class DemasiadoRapidoException extends Exception {
+
+    public DemasiadoRapidoException(String msg) {
+        super(msg);
+    }
+}
