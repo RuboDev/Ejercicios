a8962221f9f58cfaceccda55e8c0c5a85154b043
Rubén <rubololer@gmail.com>
2021-05-06 18:27:08 +0200

    Más Ejercicios de dibujo en JPanels - Graphics2D

--- CursoPildoras/frames/PruebaDibujo.java
+++ CursoPildoras/frames/PruebaDibujo.java
@@ -1,8 +1,7 @@
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
-
 import java.awt.*;
 import javax.swing.*;
-
+import java.awt.geom.*;
 public class PruebaDibujo {
     public static void main(String[] args) {
         MarcoConDibujos mimarco = new MarcoConDibujos();
@@ -25,9 +24,23 @@ class LaminaConFiguras extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // g.drawRect(50, 50, 200, 200);
-        // g.drawLine(100, 100, 300, 200);
-        g.drawArc(50, 100, 100, 200, 120, 150);
-    }
+        
+        Graphics2D g2 = (Graphics2D) g;
 
+        Rectangle2D rectangulo=new Rectangle2D.Double(100,100,200,150);
+        g2.draw(rectangulo);
+
+        Ellipse2D elipse=new Ellipse2D.Double();
+        elipse.setFrame(rectangulo);
+        g2.draw(elipse);
+        g2.draw(new Line2D.Double(100,100,300,250));
+
+        double centroenX=rectangulo.getCenterX();
+        double centroenY=rectangulo.getCenterY();
+        double radio = 150;
+
+        Ellipse2D circulo=new Ellipse2D.Double();
+        circulo.setFrameFromCenter(centroenX, centroenY, centroenX+radio, centroenY+radio);
+        g2.draw(circulo);
+    }
 }
