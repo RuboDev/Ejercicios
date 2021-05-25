d2620ababbeae835026fcbca383cbd82e3516801
Rubén <rubololer@gmail.com>
2021-05-25 16:03:52 +0200

    Menu bar with menus, nested menus, and menu items

---
+++ CursoPildoras/frames/MarcoMenu.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+
+public class MarcoMenu {
+    public static void main(String[] args) {
+        MenuFrame marco = new MenuFrame();
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+class MenuFrame extends JFrame {
+    public MenuFrame() {
+        setTitle("Prueba creacion de Menus");
+        setBounds(500, 300, 550, 400);
+        MenuLamina lamina = new MenuLamina();
+        add(lamina);
+        setVisible(true);
+    }
+}
+
+class MenuLamina extends JPanel {
+    public MenuLamina() {
+        JMenuBar mibarra = new JMenuBar();
+        JMenu archivo = new JMenu("Archivo");
+        JMenu edicion = new JMenu("Edicion");
+        JMenu herramientas = new JMenu("Herramientas");
+
+        JMenuItem guardar = new JMenuItem("Guardar");
+        JMenuItem guardarcomo = new JMenuItem("Guardar Como");
+        archivo.add(guardar);
+        archivo.add(guardarcomo);
+
+        JMenuItem copiar = new JMenuItem("Copiar");
+        JMenuItem pegar = new JMenuItem("Cortar");
+        JMenuItem cortar = new JMenuItem("Pegar");
+        JMenu opciones = new JMenu("Opciones");
+        JMenuItem opcion1 = new JMenuItem("Opcion 1");
+        JMenuItem opcion2 = new JMenuItem("Opcion 2");
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+
+        edicion.add(copiar);
+        edicion.add(pegar);
+        edicion.add(cortar);
+        edicion.addSeparator();
+        edicion.add(opciones);
+
+        JMenuItem generales = new JMenuItem("Generales");
+        herramientas.add(generales);
+
+        mibarra.add(archivo);
+        mibarra.add(edicion);
+        mibarra.add(herramientas);
+        add(mibarra);
+    }
+}
