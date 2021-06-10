a56a4814ac054c39f9d34601e60e10bf1414476f
Rubén <rubololer@gmail.com>
2021-06-10 10:45:57 +0200

    added method to get the radioButton selected

--- CursoPildoras/practicas/appCuadrosDialogo/LaminaCajas.java
+++ CursoPildoras/practicas/appCuadrosDialogo/LaminaCajas.java
@@ -1,54 +1,26 @@
 import javax.swing.*;
-import java.awt.event.*;
 
 public class LaminaCajas extends JPanel {
-    OyenteSelectores oyeSelectores = new OyenteSelectores();
+    private ButtonGroup grupo;
 
     public LaminaCajas(String titulo, String[] opciones) {
         setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), titulo));
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        ButtonGroup grupo = new ButtonGroup();
+        grupo = new ButtonGroup();
 
         for (int i = 0; i < opciones.length; i++) {
             JRadioButton bot = new JRadioButton(opciones[i]);
             add(bot);
             grupo.add(bot);
             bot.setSelected(i == 0);
+            bot.setActionCommand(opciones[i]);
         }
     }
 
-    private class OyenteSelectores implements ActionListener {
-
-        JRadioButton tipoB, tipomsg, msg;
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String cosas = e.getActionCommand();
-
-            if (cosas == "Mensaje" || cosas == "Confirmar" || cosas == "Opcion" || cosas == "Entrada") {
-
-                // boolean msgonoff = rButton.isSelected();
-                tipoB = (JRadioButton) e.getSource();
-                System.out.println(tipoB.isSelected());
-            }
-
-            if (cosas == "ERROR_MESSAGE" || cosas == "INFORMATION_MESSAGE" || cosas == "WARNING_MESSAGE"
-                    || cosas == "QUESTION_MESSAGE" || cosas == "PLAIN_MESSAGE") {
-                tipomsg = (JRadioButton) e.getSource();
-                // boolean erroronoff = rButton.isSelected();
-                System.out.println(tipomsg.isSelected());
-
-            }
-
-            if (cosas == "Cadena" || cosas == "Icono" || cosas == "Componente" || cosas == "Otros"
-                    || cosas == "Object[]") {
-                msg = (JRadioButton) e.getSource();
-                // boolean erroronoff = rButton.isSelected();
-                System.out.println(msg.isSelected());
-
-            }
-        }
-
+    public String dameSeleccion() {
+        ButtonModel miboton = grupo.getSelection();
+        String s = miboton.getActionCommand();
+        return s;
     }
 }
