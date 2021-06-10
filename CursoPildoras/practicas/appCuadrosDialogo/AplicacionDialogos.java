beb9d4f97f5b34d402c8adb66603b41c1c6dcb38
Rubén <rubololer@gmail.com>
2021-06-10 09:46:14 +0200

    totally refactored

--- CursoPildoras/practicas/appCuadrosDialogo/AplicacionDialogos.java
+++ CursoPildoras/practicas/appCuadrosDialogo/AplicacionDialogos.java
@@ -4,5 +4,6 @@ public class AplicacionDialogos {
     public static void main(String[] args) {
         MarcoDialogos marco = new MarcoDialogos();
         marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marco.setVisible(true);
     }
 }
--- CursoPildoras/practicas/appCuadrosDialogo/MarcoDialogos.java
+++ CursoPildoras/practicas/appCuadrosDialogo/MarcoDialogos.java
@@ -1,11 +1,51 @@
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
 
 public class MarcoDialogos extends JFrame {
+    private LaminaCajas lamina1, lamina2, lamina3, lamina4, lamina5, lamina6;
+
     public MarcoDialogos() {
         setTitle("Selector Cuadro de dialogos");
-        setBounds(600, 400, 600, 400);
-        LaminaPrincipal lamina = new LaminaPrincipal();
-        add(lamina);
-        setVisible(true);
+        setBounds(600, 400, 650, 450);
+        setLayout(new BorderLayout());
+
+        JPanel laminaGrid = new JPanel();
+        laminaGrid.setLayout(new GridLayout(2, 3));
+
+        String[] opcTipo = { "Mensaje", "Confirmar", "Opcion", "Entrada" };
+        lamina1 = new LaminaCajas("Tipo", opcTipo);
+
+        String[] opcTipoMsg = { "ERROR_MESSAGE", "INFORMATION_MESSAGE", "WARNING_MESSAGE", "QUESTION_MESSAGE",
+                "PLAIN_MESSAGE" };
+        lamina2 = new LaminaCajas("Tipo de Mensaje", opcTipoMsg);
+
+        String[] opcMsg = { "Cadena", "Icono", "Componente", "Otros", "Object[]" };
+        lamina3 = new LaminaCajas("Mensaje", opcMsg);
+
+        String[] opcConfir = { "DEFAULT_OPTION", "YES_NO_OPTION", "YES_NO_CANCEL_OPTION", "OK_CANCEL_OPTION" };
+        lamina4 = new LaminaCajas("Confirmar", opcConfir);
+
+        String[] opcOpcion = { "String[]", "Icon[]", "Object[]" };
+        lamina5 = new LaminaCajas("Opcion", opcOpcion);
+
+        String[] opcEntrada = { "Campo de texto", "Combo" };
+        lamina6 = new LaminaCajas("Entrada", opcEntrada);
+
+        laminaGrid.add(lamina1);
+        laminaGrid.add(lamina2);
+        laminaGrid.add(lamina3);
+        laminaGrid.add(lamina4);
+        laminaGrid.add(lamina5);
+        laminaGrid.add(lamina6);
+
+        JPanel laminaBotonMostrar = new JPanel();
+        JButton botonMostrar = new JButton("Mostrar");
+        laminaBotonMostrar.add(botonMostrar);
+
+        add(laminaGrid, BorderLayout.CENTER);
+        add(laminaBotonMostrar, BorderLayout.SOUTH);
     }
 }
