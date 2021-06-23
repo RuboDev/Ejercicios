b7f9c326e90a08b8308ab149a1e74fd9f4d1940a
Ruben <rubololer@gmail.com>
2021-06-23 16:11:01 +0200

    another way to synchronize Threads

--- CursoPildoras/Programacion_concurrente/src/usothreads/SincronizandoHilos.java
+++ CursoPildoras/Programacion_concurrente/src/usothreads/SincronizandoHilos.java
@@ -3,26 +3,40 @@ package usothreads;
 public class SincronizandoHilos {
     public static void main(String[] args) {
         HilosVarios hilo1 = new HilosVarios();
-        hilo1.start();
-        try {
-            hilo1.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        HilosVarios hilo2 = new HilosVarios();
+        HilosVarios2 hilo2 = new HilosVarios2(hilo1);
         hilo2.start();
-        try {
-            hilo2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        hilo1.start();
+
         System.out.println("Terminadas las tareas");
     }
 }
 
 class HilosVarios extends Thread {
     public void run() {
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Ejecutando hilo" + getName());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class HilosVarios2 extends Thread {
+    private Thread hilo;
+
+    public HilosVarios2(Thread hilo) {
+        this.hilo = hilo;
+    }
+
+    public void run() {
+        try {
+            hilo.join();
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
         for (int i = 0; i < 15; i++) {
             System.out.println("Ejecutando hilo" + getName());
             try {
