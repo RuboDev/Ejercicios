9641ba0c9e3fe7a3b65d3d85dfe44cd0be8b0b4e
Ruben <rubololer@gmail.com>
2021-07-06 14:46:51 +0200

    reusing prepared query to make multiples queries without rewriting sentences

--- CursoPildoras/BBDD_JDBC/src/ConsultaPreparada.java
+++ CursoPildoras/BBDD_JDBC/src/ConsultaPreparada.java
@@ -18,8 +18,22 @@ public class ConsultaPreparada {
             miSentencia.setString(1, "Deportes");
             miSentencia.setString(2, "USA");
 
-            // 4. Ejecutar y recorrer consulta
+            // 4.1 Ejecutar y recorrer consulta 1
             ResultSet rs = miSentencia.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+            rs.close();
+
+            // REUTILIZACIÓN DE CONSULTA SQL
+            System.out.println();
+            System.out.println("Ejecucion de segunda consulta");
+            System.out.println();
+            miSentencia.setString(1, "CERÁMICA");
+            miSentencia.setString(2, "CHINA");
+
+            // 4.2 Ejecutar y recorrer consulta 2
+            rs = miSentencia.executeQuery();
             while (rs.next()) {
                 System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
             }
