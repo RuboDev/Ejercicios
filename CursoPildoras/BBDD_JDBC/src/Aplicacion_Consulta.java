582c1c3b6fab032720488f96b861a660ae7e4afc
Ruben <rubololer@gmail.com>
2021-07-07 20:14:18 +0200

    now application queries the database filtering by section and country

--- CursoPildoras/BBDD_JDBC/src/Aplicacion_Consulta.java
+++ CursoPildoras/BBDD_JDBC/src/Aplicacion_Consulta.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.swing.JButton;
@@ -8,6 +9,7 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import java.awt.event.*;
 
 public class Aplicacion_Consulta {
     public static void main(String[] args) {
@@ -20,6 +22,7 @@ public class Aplicacion_Consulta {
 class MarcoAplicacion extends JFrame {
     JComboBox<String> seccion;
     JComboBox<String> pais;
+    JTextArea areaTexto;
 
     public MarcoAplicacion() {
         setBounds(600, 400, 600, 600);
@@ -44,12 +47,78 @@ class MarcoAplicacion extends JFrame {
         cargarComboBoxes();
 
         // Text Area
-        JTextArea areaTexto = new JTextArea(4, 50);
+        areaTexto = new JTextArea(4, 50);
         add(areaTexto, BorderLayout.CENTER);
 
         // Botón consulta
         JButton btnConsulta = new JButton("Consulta");
+        btnConsulta.addActionListener(new OyenteBoton());
         add(btnConsulta, BorderLayout.SOUTH);
+    }
+
+    private class OyenteBoton implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            areaTexto.setText("");
+
+            String nombreSeccion = (String) seccion.getSelectedItem();
+            String nombrePais = (String) pais.getSelectedItem();
+
+            try {
+                // 1. Crear conexión
+                Connection miConexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/prueba", "root", "");
+
+                PreparedStatement miSentencia;
+                // 2. Preparar diferentes consultas
+                if (!nombreSeccion.equals("Todos") && !nombrePais.equals("Todos")) {
+
+                    miSentencia = miConexion.prepareStatement(
+                            "SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO, PAÍSDEORIGEN FROM PRODUCTOS WHERE SECCIÓN=? AND PAÍSDEORIGEN=?");
+                    // 3.1 Establecer parámetros de consulta
+                    miSentencia.setString(1, nombreSeccion);
+                    miSentencia.setString(2, nombrePais);
+
+                } else if (nombreSeccion.equals("Todos") && !nombrePais.equals("Todos")) {
+
+                    miSentencia = miConexion.prepareStatement(
+                            "SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO, PAÍSDEORIGEN FROM PRODUCTOS WHERE PAÍSDEORIGEN=?");
+                    // 3.2 Establecer parámetros de consulta
+                    miSentencia.setString(1, nombrePais);
+
+                } else if (!nombreSeccion.equals("Todos") && nombrePais.equals("Todos")) {
+
+                    miSentencia = miConexion.prepareStatement(
+                            "SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO, PAÍSDEORIGEN FROM PRODUCTOS WHERE SECCIÓN=?");
+                    // 3.3 Establecer parámetros de consulta
+                    miSentencia.setString(1, nombreSeccion);
+
+                } else {
+
+                    miSentencia = miConexion
+                            .prepareStatement("SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO, PAÍSDEORIGEN FROM PRODUCTOS");
+
+                }
+
+                // 4.1 Ejecutar y recorrer consulta 1
+                ResultSet rs = miSentencia.executeQuery();
+                while (rs.next()) {
+                    areaTexto.append(rs.getString(1));
+                    areaTexto.append(", ");
+                    areaTexto.append(rs.getString(2));
+                    areaTexto.append(", ");
+                    areaTexto.append(rs.getString(3));
+                    areaTexto.append("€, ");
+                    areaTexto.append(rs.getString(4));
+                    areaTexto.append("\n");
+                }
+                rs.close();
+
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+
     }
 
     // Procedimiento para cargar de items los comboboxes de la UI.
