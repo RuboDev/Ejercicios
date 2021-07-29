1cbd30643a1b8b80f2b9c73840c8c08a2d6ff290
Ruben <rubololer@gmail.com>
2021-07-29 10:33:21 +0200

    getting constructor, method and field modifiers, types, and names from class name

--- CursoPildoras/Anexos/src/introspeccion/PruebaIntrospeccion.java
+++ CursoPildoras/Anexos/src/introspeccion/PruebaIntrospeccion.java
@@ -1,6 +1,8 @@
 package introspeccion;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Scanner;
 
@@ -30,10 +32,14 @@ public class PruebaIntrospeccion {
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(0);
+        }finally{
+            entrada.close();
         }
     }
 
+    //imprime todos los CONSTRUCTORES de la clase introducida por teclado
     public static void imprimirConstructores(Class<?> cl) {
+        System.out.println("##### CONSTRUCTORES DE LA CLASE: "+cl.getName()+" #####");
         Constructor<?>[] constructores = cl.getDeclaredConstructors();
         for (Constructor<?> c : constructores) {
             String nombre= c.getName();
@@ -42,10 +48,60 @@ public class PruebaIntrospeccion {
             //imprimir parámetros
             Class<?>[] tipoParams = c.getParameterTypes();
             for (int i = 0; i < tipoParams.length; i++) {
-                System.out.print(" arg"+i+", ");
+                
                 System.out.print(tipoParams[i].getName());
+                
+                if(tipoParams.length - i > 1){
+                    System.out.print(" arg"+(i)+", ");
+                }else{
+                    System.out.print(" arg"+(i));
+                }
+                
             }
             System.out.println(");");
+        }
+    }
+
+    //imprime todos los METODOS de la clase introducida por teclado
+    public static void imprimirMetodos(Class<?> cl) {
+        System.out.println("@@@@@ METODOS DE LA CLASE: "+cl.getName()+" @@@@@");
+        Method[] metodos = cl.getDeclaredMethods();
+
+        for (Method m : metodos) {
+            Class<?> tipoDevuelto = m.getReturnType();
+            String nombre = m.getName();
+
+            //imprime modificadores, tipo y nombre
+            System.out.print("   "+Modifier.toString(m.getModifiers()));
+            System.out.print("   "+tipoDevuelto.getName()+ " "+nombre+"(");
+
+            //imprime tipo de parámetros
+            Class<?>[] tipoParams = m.getParameterTypes();
+            for (int i = 0; i < tipoParams.length; i++) {
+                System.out.print(tipoParams[i].getName());
+                
+                if(tipoParams.length - i > 1){
+                    System.out.print(" arg"+(i)+", ");
+                }else{
+                    System.out.print(" arg"+(i));
+                }
+            }
+            System.out.println(");");
+        }
+    }
+
+    public static void imprimirCampos(Class<?> cl) {
+        System.out.println("%%%%% CAMPOS DE LA CLASE: "+cl.getName()+" %%%%%");
+        Field[] campos = cl.getDeclaredFields();
+
+        for (Field c : campos) {
+            String nombre = c.getName();
+            Class<?> tipo = c.getType();
+
+            //imprime modificadores, tipo y nombre
+            System.out.print("   "+Modifier.toString(c.getModifiers()));
+            System.out.print(" "+tipo.getName()+ " "+nombre+";");
+            System.out.println();
         }
     }
 }
